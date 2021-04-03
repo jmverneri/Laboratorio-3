60f5d91faf297a63dcded87f771b095e5d0a98ab
jmverneri <jamartinezverneri@gmail.com>
2021-04-03 19:27:04 -0300

    EJ3

---
+++ Guia 3/src/com/utn/Ej3/Figure.java
@@ -0,0 +1,11 @@
+package com.utn.Ej3;
+
+public class Figure {
+    protected String color;
+    protected boolean painted;
+
+    public Figure(String color, boolean painted){
+        this.color= color;
+        this.painted= painted;
+    }
+}
